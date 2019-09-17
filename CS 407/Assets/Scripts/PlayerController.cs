94c3c03b79edd560a9f16a27fd11c151d6ed58cf
Sung <bsung@purdue.lcl>
2019-09-17 13:42:42 -0400

    Collision detection player movement

--- CS 407/Assets/Scripts/PlayerController.cs
+++ CS 407/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,10 @@ public class PlayerController : MonoBehaviour
 
     public float moveSpeed = 5f;
 
+    private RaycastHit2D hit;
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,8 +34,17 @@ public class PlayerController : MonoBehaviour
             transform.localScale = new Vector3(-1,1,1);
         }
 
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveSpeed * moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if(hit.collider == null)
+        {
+            transform.Translate(0,moveDelta.y * Time.deltaTime * moveSpeed, 0);
+        }
 
-        transform.Translate(moveSpeed * moveDelta * Time.deltaTime);
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x,0), Mathf.Abs( moveSpeed * moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            transform.Translate(moveDelta.x * Time.deltaTime * moveSpeed, 0,0);
+        }
 
     }
 }
