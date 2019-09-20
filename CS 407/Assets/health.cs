29569edbd233bb9bcada9578207dcf45a0641e02
Conner Isaacs <cjisaacs@att.net>
2019-09-20 03:01:26 -0400

    Added Health bar and base for leveling

---
+++ CS 407/Assets/health.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class health : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get the player
+        PlayerController player = GameObject.Find("Player").GetComponent<PlayerController>();
+
+        //receive the player's health from their stats array
+        double playerHealth = (double)player.stats[0];
+
+        //receive the player's max health from their stats array
+        double playerMaxHealth = (double)player.stats[1];
+
+        //Calculate the percent and multiply it by the initial width of the bar (e.g. at 100% it will equal 223.813)
+        double percent = playerHealth / playerMaxHealth * 223.813;
+        RectTransform rt = this.GetComponent<RectTransform>();
+
+        //set new size of the health bar
+        rt.sizeDelta = new Vector2((float)percent, (float)33.152);
+    }
+}
