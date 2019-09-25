76ff57a76854605bb540d49bc360ccf0874f0eb5
bsung03 <bsung@purdue.edu>
2019-09-25 13:04:53 -0400

    space interact

--- CS 407/Assets/Scripts/PlayerLooking.cs
+++ CS 407/Assets/Scripts/PlayerLooking.cs
@@ -5,7 +5,40 @@ using UnityEngine;
 public class PlayerLooking : MonoBehaviour
 {
 
-    private Vector2 current_Mouse_Look;
+    private float timeBtwAttack;
+    public Transform interactPos;
+    public float interactRange;
+    public LayerMask interactLayerMask;
+    private Vector3 Mouse_current_position;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("space");
+            Mouse_interact_pos();
+            Collider2D[] interact = Physics2D.OverlapCircleAll(Mouse_current_position, interactRange, interactLayerMask);
+            for(int i = 0;i < interact.Length; i++)
+            {
+                Debug.Log("interact");
+            }
+        }
+    }
+
+    private void Mouse_interact_pos()
+    {
+        Mouse_current_position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(interactPos.position, interactRange);
+    }
+
+
+
+    /*private Vector2 current_Mouse_Look;
     private Vector3 Player_current_position;
     private Vector3 Mouse_current_position;
 
@@ -29,5 +62,5 @@ public class PlayerLooking : MonoBehaviour
         if(hit != null){
             Debug.Log("hithit");
         }
-    }
+    }*/
 }
