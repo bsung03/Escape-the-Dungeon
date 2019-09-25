aab42c6d6e30536b18df8ba10ede371a5d93b6f4
bsung03 <bsung@purdue.edu>
2019-09-25 15:56:12 -0400

    fixed radius around player

--- CS 407/Assets/Scripts/PlayerLooking.cs
+++ CS 407/Assets/Scripts/PlayerLooking.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class PlayerLooking : MonoBehaviour
 {
 
-    private float timeBtwAttack;
-    public Transform interactPos;
+    public Vector3 interactPos;
     public float interactRange;
     public LayerMask interactLayerMask;
     private Vector3 Mouse_current_position;
+    private Vector3 look_direction;
 
     void Update()
     {
@@ -17,7 +17,7 @@ public class PlayerLooking : MonoBehaviour
         {
             Debug.Log("space");
             Mouse_interact_pos();
-            Collider2D[] interact = Physics2D.OverlapCircleAll(Mouse_current_position, interactRange, interactLayerMask);
+            Collider2D[] interact = Physics2D.OverlapCircleAll(interactPos, interactRange, interactLayerMask);
             for(int i = 0;i < interact.Length; i++)
             {
                 Debug.Log("interact");
@@ -28,39 +28,14 @@ public class PlayerLooking : MonoBehaviour
     private void Mouse_interact_pos()
     {
         Mouse_current_position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        look_direction = Mouse_current_position - transform.position;
+        look_direction.Normalize();
+        interactPos = transform.position + look_direction;
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(interactPos.position, interactRange);
+        Gizmos.DrawWireSphere(interactPos, interactRange);
     }
-
-
-
-    /*private Vector2 current_Mouse_Look;
-    private Vector3 Player_current_position;
-    private Vector3 Mouse_current_position;
-
-    private BoxCollider2D boxCollider;
-    private RaycastHit2D hit;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        boxCollider = GetComponent<BoxCollider2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Mouse_current_position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //current_Mouse_Look = new Vector2(Input.GetAxis("Horizontal2"),Input.GetAxis("Vertical2"));
-        Player_current_position = transform.position;
-
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, Mouse_current_position, 0f, LayerMask.GetMask("collect"));
-        if(hit != null){
-            Debug.Log("hithit");
-        }
-    }*/
 }
