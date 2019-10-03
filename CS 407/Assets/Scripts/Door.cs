526aa1c6861c14113a6f8ac47370498c9dc12280
Nada16 <n.al-noory@hotmail.com>
2019-10-02 23:18:59 -0400

    Openning doors

---
+++ CS 407/Assets/Scripts/Door.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Door : MonoBehaviour
+{
+    public GameObject player;
+    public SpriteRenderer open_render1, open_render2;
+    bool open = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+
+        if (Input.GetKeyDown(KeyCode.P) && dist <= 2 && player.GetComponent<PlayerController>().keys >= 1 && !open)
+        {
+            player.GetComponent<PlayerController>().keys--;
+            print("Opened Door");
+            this.GetComponent<SpriteRenderer>().enabled = false;
+            open_render1.enabled = true;
+            open_render2.enabled = true;
+            open = true;
+        }
+    }
+
+
+}
