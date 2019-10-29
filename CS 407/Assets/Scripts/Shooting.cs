8ea9e901eeb6051ad387c9d18145d8ecee094d14
Jisoo <jslev123@gmail.com>
2019-10-29 13:57:38 -0400

    asd

--- CS 407/Assets/Scripts/Shooting.cs
+++ CS 407/Assets/Scripts/Shooting.cs
@@ -7,31 +7,60 @@ public class Shooting : MonoBehaviour
     public Transform firePoint;
     public GameObject bulletPrefab;
 
-    public AudioSource shoots;
+    public GameObject bombPrefab;
+
+    public GameObject shoots;
+    public GameObject bombs;
+
+    private AudioSource a;
+    private AudioSource b;
+
 
     public float bulletForce = 20f;
 
     private float timeBtwAttack = 0;
     public float cooldown;
+    public float skillcooldown;
+    private float timeskill = 0;
 
+    void Start()
+    {
+        a = shoots.GetComponent<AudioSource>();
+        b = bombs.GetComponent<AudioSource>();
+
+
+    }
     void Update()
     {
-        if (Time.time > timeBtwAttack)
+        if (Input.GetButtonDown("Fire1") && Time.timeScale == 1f)
         {
-            if (Input.GetButtonDown("Fire1") && Time.timeScale == 1f)
-            {
-                shoots.Play();
+            if (Time.time > timeBtwAttack) {
                 Shoot();
                 timeBtwAttack = Time.time + cooldown;
-
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && Time.timeScale == 1f)
+        {
+            if (Time.time > timeskill)
+            {
+                Bomb();
+                timeskill = Time.time + skillcooldown;
             }
         }
     }
 
     void Shoot()
     {
+        a.Play();
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.right * bulletForce, ForceMode2D.Impulse);
+    }
+    void Bomb()
+    {
+        b.Play();
+        GameObject bomb = Instantiate(bombPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bomb.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint.right * bulletForce, ForceMode2D.Impulse);
     }
 }
