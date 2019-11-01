38b1eb4df8f77e61232a3af1f46f1187835d86f0
Nada16 <n.al-noory@hotmail.com>
2019-10-31 22:25:33 -0400

    allow gunner to open doors

--- CS 407/Assets/Scripts/Door.cs
+++ CS 407/Assets/Scripts/Door.cs
@@ -16,7 +16,11 @@ public class Door : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindWithTag("Player");
+        player = GameObject.Find("Player(Clone)");
+        if(player == null)
+        {
+            player = GameObject.Find("Player 1(Clone)");
+        }
     }
 
     // Update is called once per frame
